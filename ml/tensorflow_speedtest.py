8a3b53842832511eeb623226c71ae78993909182
WillMeagher <willfmeagher@gmail.com>
2022-08-31 01:30:34 -0400

    Update to use eye status checking

--- ml/tensorflow_speedtest.py
+++ ml/tensorflow_speedtest.py
@@ -1,3 +1,4 @@
+from selectors import EpollSelector
 import cv2
 import time
 import numpy as np
@@ -7,18 +8,30 @@ def main():
     num_frames = 100
     cur_frames = 0
 
-    global output_details
-    global input_details
-    global interpreter
+    global output_details_eye
+    global input_details_eye
+    global interpreter_eye
+
+    global output_details_gaze
+    global input_details_gaze
+    global interpreter_gaze
 
     MODEL_PATH = '/home/pi/Downloads/eye_tracking_device/ml/models/'
-    MODEL_NAME = 'model_3.38.tflite'
+    MODEL_NAME_GAZE = 'model_3.38.tflite'
+    MODEL_NAME_EYE = '1661920883270_0.00000_model.tflite'
     
-    interpreter = Interpreter(model_path=MODEL_PATH + MODEL_NAME)
-    interpreter.allocate_tensors()
+    interpreter_gaze = Interpreter(model_path=MODEL_PATH + MODEL_NAME_GAZE)
+    interpreter_gaze.allocate_tensors()
 
-    input_details = interpreter.get_input_details()
-    output_details = interpreter.get_output_details()
+    input_details_gaze = interpreter_gaze.get_input_details()
+    output_details_gaze = interpreter_gaze.get_output_details()
+
+
+    interpreter_eye = Interpreter(model_path=MODEL_PATH + MODEL_NAME_EYE)
+    interpreter_eye.allocate_tensors()
+
+    input_details_eye = interpreter_eye.get_input_details()
+    output_details_eye = interpreter_eye.get_output_details()
 
     # start video
     cap = cv2.VideoCapture(0)
@@ -32,12 +45,22 @@ def main():
         ret, img = cap.read()
 
         eyes = prepare_img(img)
-        
-        prediction = predict(eyes)
 
-        prediction = process_prediction(prediction)
-        
-        print("x=%6.2f" %prediction[0], "y=%6.2f" %prediction[1])
+        left, right = get_eyes(eyes)
+
+        left_prediction, right_prediction = predict_eye(left), predict_eye(right)
+        left_prediction, right_prediction = process_prediction_eye(left_prediction), process_prediction_eye(right_prediction)
+
+        if left_prediction == 1 and right_prediction == 1:
+
+            prediction_gaze = predict_gaze(eyes)
+            prediction_gaze = process_prediction_gaze(prediction_gaze)
+            
+            print("x=%6.2f" %prediction_gaze[0], "y=%6.2f" %prediction_gaze[1])
+
+        else:
+            print("No eyes detected")
+
 
         cur_frames += 1
 
@@ -71,19 +94,46 @@ def prepare_img(img):
     return eyes
 
 
-def predict(img):
-    input_data = np.reshape(np.array([img], dtype=np.float32), (1, 15, 60, 1))
+def get_eyes(img):
+    left = img[:, :img.shpae[1] // 2]
+    right = img[:, img.shape[1] // 2:]
 
-    interpreter.set_tensor(input_details[0]['index'], input_data)
+    return left, right
+
+
+def process_prediction_eye(prediction):
+    prediction = prediction[0]
+
+    prediction = round(prediction)
+
+    return prediction
+
+
+def predict_eye(eye):
+    input_data = np.reshape(np.array([eye], dtype=np.float32), (1, 15, 30, 1))
+
+    interpreter_eye.set_tensor(input_details_eye[0]['index'], input_data)
     
-    interpreter.invoke()
+    interpreter_eye.invoke()
 
-    output_data = interpreter.get_tensor(output_details[0]['index'])
+    output_data = interpreter_gaze.get_tensor(output_details_eye[0]['index'])
 
     return output_data
 
 
-def process_prediction(prediction):
+def predict_gaze(img):
+    input_data = np.reshape(np.array([img], dtype=np.float32), (1, 15, 60, 1))
+
+    interpreter_gaze.set_tensor(input_details_gaze[0]['index'], input_data)
+    
+    interpreter_gaze.invoke()
+
+    output_data = interpreter_gaze.get_tensor(output_details_gaze[0]['index'])
+
+    return output_data
+
+
+def process_prediction_gaze(prediction):
     prediction = prediction[0]
 
     prediction = (prediction * 90) - 45
